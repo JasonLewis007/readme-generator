f6a0a8404d48eba04857228aeaf0e1ce28a657a5
Jason Lewis <pensfan5050@gmail.com>
2023-04-11 17:47:18 -0400

    4th commit-added code to generate license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,4 +17,23 @@ function generateMarkdown(data) {
 `;
 }
 
+const fs = require('fs');
+
+const generatelicense = (type) => {
+  let color;
+  if (type === "MIT") color = "goldenrod";
+  if (type === "Apache") color = "blue";
+  if (type === "MPL") color = "green";
+  if (type === "CC") color = "purple";
+  if (type === "NONE") color = "red";
+
+  return (
+    `
+## License
+<img src="https://img.shields.io/badge/license-${type}-${color}" alt="badge-${type}" height="44" />
+  `
+  );
+
+};
+
 module.exports = generateMarkdown;
